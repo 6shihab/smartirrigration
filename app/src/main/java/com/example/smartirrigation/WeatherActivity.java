ba540afeb8f4c6bdf8fc70fef4c61c8b4fa0ac7a
Shihabul Hasan Shihab <6shihab@gmail.com>
2022-11-19 22:09:41 +0600

    weatherApi Updated

--- app/src/main/java/com/example/smartirrigation/WeatherActivity.java
+++ app/src/main/java/com/example/smartirrigation/WeatherActivity.java
@@ -1,11 +1,34 @@
 package com.example.smartirrigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String DEGREE_SIGN="\u2103";
+    public StringBuilder maxMinStringBuilder;
+    TextView tempTextview,maxMinTextview,precipitationTextview,precipitationTypeTextview,currentConditionTextView,weatherDescriptionTextView;
+
     String moisture="0";
     String ph="0";
     String temperature="0";
@@ -14,6 +37,12 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        tempTextview=findViewById(R.id.tempTextView);
+        maxMinTextview=findViewById(R.id.maxMinTextviewID);
+        precipitationTextview=findViewById(R.id.precipitationTextviewID);
+        precipitationTypeTextview=findViewById(R.id.precipitationTypeTextviewID);
+        currentConditionTextView=findViewById(R.id.currentConditionTextViewID);
+        weatherDescriptionTextView=findViewById(R.id.weatherDescriptionTextViewID);
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
@@ -22,6 +51,98 @@ public class WeatherActivity extends AppCompatActivity {
             temperature = bundle.getString("temperature");
         }
 
+        maxMinStringBuilder=new StringBuilder();
+        callVisualCrossingWeatherAPI();
+
+
+
+
+        BottomNavigationView bottomNav = (BottomNavigationView)findViewById(R.id.bottomNagivationView);
+        bottomNav.setSelectedItemId(R.id.recieve);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.recieve:
+                        try{
+                            Intent intent = new Intent(WeatherActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(0,0);
+                        }catch (Exception e ){
+                            Toast.makeText(WeatherActivity.this, "Data not available", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    case R.id.data:
+                        try{
+                            Intent intent = new Intent(WeatherActivity.this,dataActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(0,0);
+                        }catch (Exception e ){
+                            Toast.makeText(WeatherActivity.this, "Data not available", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    case R.id.about:
+                        try{
+                            Intent intent = new Intent(WeatherActivity.this,AboutActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(0,0);
+                        }catch (Exception e ){
+                            Toast.makeText(WeatherActivity.this, "Data not available", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
+
+    public void callVisualCrossingWeatherAPI(){
+        String lat;
+        String lon;
+        lat="24.0129";
+        lon="89.2591";
+        JSONArray jsonDays;
+
+        String url="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+lat+","+lon+"/next7days?unitGroup=metric&key=Y3MS4XF2KV3B9A727FE98P5P7&contentType=json";
+
+        RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonDays=response.getJSONArray("days");
+                    String temperature=Integer.toString((int)Double.parseDouble(jsonDays.getJSONObject(0).getString("temp")));
+                    tempTextview.setText(temperature);
+                    maxMinStringBuilder.append(jsonDays.getJSONObject(0).getString("tempmax")+"\u2103");
+                    maxMinStringBuilder.append(" / ");
+                    maxMinStringBuilder.append(jsonDays.getJSONObject(0).getString("tempmin")+"\u2103");
+                    String dayOfTheWeek = new SimpleDateFormat("EEE").format(new Date());
+                    maxMinStringBuilder.append("  "+dayOfTheWeek);
+                    maxMinTextview.setText(maxMinStringBuilder);
+                    precipitationTextview.setText("Precipitation: "+jsonDays.getJSONObject(0).getString("precipprob")+"%");
+                    if (jsonDays.getJSONObject(0).getString("preciptype")!="null"){
+                        precipitationTypeTextview.setText("Type: "+jsonDays.getJSONObject(0).getString("preciptype"));
+                    }
+                    else {
+                        precipitationTypeTextview.setText("Type: No Rain");
+                    }
+                    currentConditionTextView.setText(jsonDays.getJSONObject(0).getString("conditions"));
+                    weatherDescriptionTextView.setText(response.getString("description"));
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"Error Occur",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"Error Occur",Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(request);
+    }
+
+
 }
